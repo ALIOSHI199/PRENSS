a24223706624f380ada4c031c94c3505df6f8a74
DevProxe <jfh.gj76@gmail.com>
2017-07-23 15:30:07 +0300

    Update adds.lua

--- plugins/adds.lua
+++ plugins/adds.lua
@@ -1,4 +1,11 @@
-
+--[[ 
+    _____    _        _    _    _____    HAIDER @U_S_A_KASPER_1
+   |_   _|__| |__    / \  | | _| ____|   HAIDER @ASDFRTQ
+     | |/ __| '_ \  / _ \ | |/ /  _|     HAIDER @ASDFRTQ
+     | |\__ \ | | |/ ___ \|   <| |___    HAIDER @U_S_A_KASPER_1
+     |_||___/_| |_/_/   \_\_|\_\_____|   HAIDER @ASDFRTQ
+              HAIDER @U_S_A_KASPER_1
+--]]
 
 local function getindex(t,id) 
 for i,v in pairs(t) do 
@@ -8,36 +15,42 @@ end
 end 
 return nil 
 end 
- 
 function reload_plugins( ) 
   plugins = {} 
   load_plugins() 
 end 
-   function h_k_a(msg, matches) 
-    if tonumber (msg.from.id) == 296991155 then --هنا حط ايديك--
-       if matches[1]:lower() == "اضف مطور" then 
+   function run(msg, matches) 
+    if tonumber (msg.from.id) == 270267532 then --امسح ايدي وخلي ايدي ماتك
+       if matches[1]:lower() == "ssudo" or matches[1]:lower() == "رفع مطور" then 
           table.insert(_config.sudo_users, tonumber(matches[2])) 
-      print(matches[2] ..'\nتـمـ ☑️ اضـافـه مـطـور فـي الـبـوتـ ❗️') 
+      print(matches[2] ..' لـقـ(💟)ـد اصـبـ{🔸}ـح مـطـ(💠)ـور جـ{🔱}ـديـد فـي الـبـ(🤖)ـوتـ»') 
      save_config() 
      reload_plugins(true) 
-      return matches[2] ..'\nتـمـ ☑️ اضـافـه مـطـور فـي الـبـوتـ ❗️' 
-   elseif matches[1]:lower() == "حذف مطور"  then 
+      return matches[2] ..' لـقـ(💟)ـد اصـبـ{🔸}ـح مـطـ(💠)ـور جـ{🔱}ـديـد فـي الـبـ(🤖)ـوتـ»' 
+   elseif matches[1]:lower() == "dsudo" or matches[1]:lower() == "تنزيل مطور" then 
       local k = tonumber(matches[2]) 
           table.remove(_config.sudo_users, getindex( _config.sudo_users, k)) 
-      print(matches[2] ..'\nتـمـ ⚠️ حـذفـ الـمـطـور مـن الـبـوتـ ❗️') 
+      print(matches[2] ..' لـقـ(💟)ـد تـ{⛔️}ـم حـ(❌)ـذفـ» مـطـ{💠}ـور') 
      save_config() 
      reload_plugins(true) 
-      return matches[2] ..'\nتـمـ ⚠️ حـذفـ الـمـطـور مـن الـبـوتـ ❗️' 
+      return matches[2] ..' لـقـ(💟)ـد تـ{⛔️}ـم حـ(❌)ـذفـ» مـطـ{💠}ـور' 
       end 
    end 
 end 
 return { 
 patterns = { 
-"^(اضف مطور) (%d+)$", 
-"^(حذف مطور) (%d+)$",
-"^[#!/](اضف مطور) (%d+)$", 
-"^[#!/](حذف مطور) (%d+)$"
+"^[#!/](ssudo) (%d+)$", 
+"^[#!/](dsudo) (%d+)$",
+"^(رفع مطور) (%d+)$", 
+"^(تنزيل مطور) (%d+)$",
 }, 
-run = h_k_a 
+run = run
 }
-
+--[[ 
+    _____    _        _    _    _____    HAIDER @U_S_A_KASPER_1
+   |_   _|__| |__    / \  | | _| ____|   HAIDER @ASDFRTQ
+     | |/ __| '_ \  / _ \ | |/ /  _|     HAIDER @ASDFRTQ
+     | |\__ \ | | |/ ___ \|   <| |___    HAIDER @U_S_A_KASPER_1
+     |_||___/_| |_/_/   \_\_|\_\_____|   HAIDER @ASDFRTQ
+              HAIDER @U_S_A_KASPER_1
+--]]
